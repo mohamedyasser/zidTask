60934e72340211c38cbdc11609c14e6c560eea85
Mohamed Yasser <yasoesr@gmail.com>
2022-12-01 23:53:27 +0200

    change to resources

--- app/Http/Controllers/ItemController.php
+++ app/Http/Controllers/ItemController.php
@@ -4,46 +4,31 @@ namespace App\Http\Controllers;
 
 use App\Http\Requests\StoreItemRequest;
 use App\Http\Requests\UpdateItemRequest;
+use App\Http\Resources\ItemResource;
 use App\Models\Item;
-use App\Serializers\ItemSerializer;
-use App\Serializers\ItemsSerializer;
 use Illuminate\Http\JsonResponse;
-use Illuminate\Http\Request;
-use League\CommonMark\CommonMarkConverter;
 
 class ItemController extends Controller
 {
     public function index()
     {
-        $items = Item::all();
-
-        return JsonResponse::create(['items' => (new ItemsSerializer($items))->getData()]);
+        return ItemResource::collection(Item::all());
     }
 
     public function store(StoreItemRequest $request)
     {
-        $item = Item::create($request->validated());
-
-        $serializer = new ItemSerializer($item);
-
-        return new JsonResponse(['item' => $serializer->getData()]);
+        return ItemResource::make(Item::create($request->validated()));
     }
 
     public function show(Item $item)
     {
-        $serializer = new ItemSerializer($item);
-
-        return new JsonResponse(['item' => $serializer->getData()]);
+        return ItemResource::make($item);
     }
 
     public function update(UpdateItemRequest $request, Item $item): JsonResponse
     {
         $item->fill($request->validated())->save();
 
-        return new JsonResponse(
-            [
-                'item' => (new ItemSerializer($item))->getData()
-            ]
-        );
+        return ItemResource::make($item);
     }
 }
